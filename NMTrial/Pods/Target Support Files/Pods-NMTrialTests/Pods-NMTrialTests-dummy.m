cdfc0dcb16538508810241adec7792f4bd9f03b1
admin <admin@Ranvijay-mac.local>
2016-10-04 19:08:35 +0530

    Basic project setup & integrating alamofire

---
+++ NMTrial/Pods/Target Support Files/Pods-NMTrialTests/Pods-NMTrialTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NMTrialTests : NSObject
+@end
+@implementation PodsDummy_Pods_NMTrialTests
+@end
---
+++ NMTrial/Pods/Target Support Files/Pods-NMTrialUITests/Pods-NMTrialUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NMTrialUITests : NSObject
+@end
+@implementation PodsDummy_Pods_NMTrialUITests
+@end
